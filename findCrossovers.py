1ee590b847084666007ae977d3ffc3392b87d9d6
Peter Zukerman <31905508+peterzukerman@users.noreply.github.com>
2017-09-12 18:30:52 -0500

    Add files via upload

---
+++ findCrossovers.py
@@ -0,0 +1,11 @@
+
+stepmania_file = open('landscape.sm', 'r')
+
+currentLineText = ""
+currentLineNumber = 0
+
+print(stepmania_file.readline())
+for line in stepmania_file:
+	currentLineText = line
+	if :
+		pass
