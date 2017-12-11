f0f18c9b0e76269054f0b053208634127dd73822
Junbin Park <junbin.park@gmail.com>
2017-12-11 03:49:28 -0600

    Reformatted for python3

--- dnc_dnc_rev.py
+++ dnc_dnc_rev.py
@@ -1,56 +1,52 @@
 import os
 import patternFinder as pf
 
+
 def initialize_notes(file_directory):
-	"""
-	Initializing notes depending on song selected in 'file_directory'
-	input: file_directory - the directory of file to be used.
-	output: returns array with each containing 4 digits.
-	"""
-	notes = []
-	file_sm = open(str(file_directory), 'r')
-	for line in file_sm:
-		#notesByMeasures = []
-		try:
-			int(line[0])
-			int(line[1])
-			int(line[2])
-			int(line[3])
-			notes.append(line[:4])
-		except:
-			pass
-		# if "measure" in line:
-		# 	measure += 1
-		# 	notesByMeasures.append("")
-		# 	notesByMeasures[measure].append(line)
-            # if len(line) == 6
-            # print line[:4]
-	#print notes
-	return notes
+    """
+    Initializing notes depending on song selected in 'file_directory'
+    input: file_directory - the directory of file to be used.
+    output: returns array with each containing 4 digits.
+    """
+    notes = []
+    file_sm = open(str(file_directory), 'r')
+    for line in file_sm:
+        #notesByMeasures = []
+        try:
+            int(line[0])
+            int(line[1])
+            int(line[2])
+            int(line[3])
+            notes.append(line[:4])
+        except:
+            pass
+    return notes
+
 
 def create_list_songs():
-	"""
-	Creates list of songs refering to 'Songs' directory. 
-	output : list of songs available in 'Songs' directory
-	"""
-	avail_songs = os.walk("./Songs").next()[1]
-	print "\tlist of songs available"
-	for idx, songs in enumerate(avail_songs):
-		print '\t\t', idx, ' : ', songs
-	return avail_songs
+    """
+    Creates list of songs refering to 'Songs' directory. 
+    output : list of songs available in 'Songs' directory
+    """
+    avail_songs = next(os.walk("./Songs"))[1]
+    print ("\tlist of songs available")
+    for idx, songs in enumerate(avail_songs):
+        print ('\t\t' + str(idx) + ' : ' + songs)
+    return avail_songs
+
 
 def check_yes_no():
     """
     checks if user input is yes('y') or no('n'). Calls function itself if invalid input entered.
     output : boolean. True if 'y' False if 'n'
     """
-    input = raw_input()
-    if input == 'y':
+    user_input = input()
+    if user_input == 'y':
         return True
-    elif input == 'n':
+    elif user_input == 'n':
         return False
     else:
-        print "Wrong input"
+        print ("Wrong input")
 
 
 def choose_songs():
@@ -59,32 +55,33 @@ def choose_songs():
     output : directory of chosen song.
     """
     list_of_songs = create_list_songs()
-    input = raw_input("\t Choose song to play :")
+    user_input = input("\t Choose song to play :")
     try:
-		int(input)
+        int(user_input)
     except:
-		print "Wrong Input"
-    while int(input) > len(list_of_songs) - 1:
-		input = raw_input("\t Wrong input, choose song to play :")
-    print "'" + list_of_songs[int(input)] + "'" + " was chosen. Wold you like to continue? (y/n)"
+        print ("Wrong Input")
+    while int(user_input) > len(list_of_songs) - 1:
+        user_input = input("\t Wrong input, choose song to play :")
+    print ("'" + list_of_songs[int(user_input)] + "'" +
+           " was chosen. Wold you like to continue? (y/n)")
     if check_yes_no():
-		directory = "./Songs/"
-		directory += list_of_songs[int(input)]
-		directory += '/'
-		files = os.listdir(directory)
-		for file in files:
-			if str(file).endswith(".sm") or str(file).endswith(".ssc"):
-				directory += file
-				return str(directory)
+        directory = "./Songs/"
+        directory += list_of_songs[int(user_input)]
+        directory += '/'
+        files = os.listdir(directory)
+        for file in files:
+            if str(file).endswith(".sm") or str(file).endswith(".ssc"):
+                directory += file
+                return str(directory)
     else:
         return choose_songs()
 
 
-
 def main():
-	file = choose_songs()
-	notes = initialize_notes(file)
-	pf.detect_crossover(notes)
+    file = choose_songs()
+    notes = initialize_notes(file)
+    pf.detect_crossover(notes)
+
 
 main()
 
@@ -121,4 +118,5 @@ example crossovers
 	basically, if we just have the boolean variable deciding which foot it is, then for example if the left foot is already down and the next arrow is a left arrow, we know it's a crossover
 there are still a couple of ambiguous cases, but that makes it a ton easier
 
+#note: Initial Consonant Game
 """
--- patternFinder.py
+++ patternFinder.py
@@ -1,48 +1,48 @@
 
 def detect_crossover(notesByMeasures):
-	foot = False  # false = left foot, true = right foot
-	arrow = 0
-	firstNote = False
-	count = 0;
-	f = open('output.txt', 'w')
-	for x in range(0, len(notesByMeasures)):
-		arrow = check_step(notesByMeasures[x])
-		if arrow == -1:
-			#print notesByMeasures[x], '\t', arrow
-			continue
-		if firstNote == False:
-			if arrow == 0:  # determines which foot hits first note of song
-				foot = False
-			else:
-				foot = False
-			firstNote = False
-		if foot and check_step(notesByMeasures[x + 1]) == 3:
-			count += 1
-			print "Crossover at line: ", x + 1
-			f.write("Crossover at line: " + str(x + 1) + '\n')
-		if not foot and check_step(notesByMeasures[x + 1]) == 0:
-			count += 1
-			print "Crossover at line: ", x + 1
-			f.write("Crossover at line: " + str(x + 1) + '\n')
-	print "check crossover done"
-	print "number of crossovers", count
-	f.write("number of crossovers: " + str(count))
-	f.close
+    foot = False  # false = left foot, true = right foot
+    arrow = 0
+    firstNote = False
+    count = 0
+    f = open('output.txt', 'w')
+    for x in range(0, len(notesByMeasures)):
+        arrow = check_step(notesByMeasures[x])
+        if arrow == -1:
+            # print notesByMeasures[x], '\t', arrow
+            continue
+        if firstNote == False:
+            if arrow == 0:  # determines which foot hits first note of song
+                foot = False
+            else:
+                foot = False
+            firstNote = False
+        if foot and check_step(notesByMeasures[x + 1]) == 3:
+            count += 1
+            print ("Crossover at line: ", x + 1)
+            f.write("Crossover at line: " + str(x + 1) + '\n')
+        if not foot and check_step(notesByMeasures[x + 1]) == 0:
+            count += 1
+            print ("Crossover at line: ", x + 1)
+            f.write("Crossover at line: " + str(x + 1) + '\n')
+    print ("check crossover done")
+    print ("number of crossovers" + str(count))
+    f.write("number of crossovers: " + str(count))
+    f.close
 
 
 def check_step(line):
-	"""
-	checks left/up/right/down/leftright/updown
-	"""
-	if line.count("0") == 4:
-		return -1  # returns -1 if there are no notes on the line
-	for x in range(0, len(line)):
-		if line[x] == '0':  # if 0, keep going until there's a note
-			continue
-		# if 1 and only 1 note, return the pos of note
-		elif line[x] == '1' and line.count('1') == 1:
-			return x
-		elif line.count('1') > 1:  # a jump
-			return -1
+    """
+    checks left/up/right/down/leftright/updown
+    """
+    if line.count("0") == 4:
+        return -1  # returns -1 if there are no notes on the line
+    for x in range(0, len(line)):
+        if line[x] == '0':  # if 0, keep going until there's a note
+            continue
+        # if 1 and only 1 note, return the pos of note
+        elif line[x] == '1' and line.count('1') == 1:
+            return x
+        elif line.count('1') > 1:  # a jump
+            return -1
 
-	return -1
+    return -1
